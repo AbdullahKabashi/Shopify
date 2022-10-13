9204758c7bbb3ffbfe8e6410a23b1a74d6d2fba1
Abdullah Kabashi <abdullahkabashi01@gmail.com>
2022-10-13 22:32:18 +0200

    ex

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let courseName = 'cedx'
+window.alert(courseName)
+
+// const name ='Mose';
+// const age = 10;
+// const sentence =
+// `My cat ${name} is ${age * 4+17}years old in cat years`
+// console.log(sentence)
+
+const num1 = 5;
+const num2 = 7;
+const sentence = 
+`Adding ${num1} and ${num2} gives ${num1 + num2}`
+console.log(sentence)
---
+++ javascript/script2.js
@@ -0,0 +1,8 @@
+let laptopName = 'dell'
+console.log(laptopName)
+
+let tv = 100.75
+let chips = 0.25
+const sum = tv + chips;
+console.log(sum)
+console.log(typeof sum);
